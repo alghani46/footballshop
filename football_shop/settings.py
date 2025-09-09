66f261be1e7a0d132ff4823cec0d7d19843e0ca6
alghani46 <alghani.gucci@gmail.com>
2025-09-09 17:02:53 +0700

    Update settings.py

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -81,30 +81,30 @@ WSGI_APPLICATION = 'football_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# Database configuration
 if PRODUCTION:
-    # Production: gunakan PostgreSQL dengan kredensial dari environment variables
+    # Production: use PostgreSQL with credentials from environment variables
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('muhammad.rifqi46'),
-            'USER': os.getenv('muhammad.rifqi46'),
-            'PASSWORD': os.getenv('NM7qyAzc'),
-            'HOST': os.getenv('152.118.29.139'),
-            'PORT': os.getenv('5432'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
             'OPTIONS': {
                 'options': f"-c search_path={os.getenv('SCHEMA', 'public')}"
             }
         }
     }
 else:
-    # Development: gunakan SQLite
+    # Development: use SQLite
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
