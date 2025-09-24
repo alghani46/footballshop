43ebd0e1e7f11836222aea94646de31b33313f55
alghani46 <alghani.gucci@gmail.com>
2025-09-24 07:04:39 +0700

    Assaigment 4
    Adding login systems

--- main/models.py
+++ main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Product(models.Model):
     name = models.CharField(max_length=100)  
     price = models.IntegerField()            
@@ -7,6 +7,6 @@ class Product(models.Model):
     thumbnail = models.URLField()            
     category = models.CharField(max_length=50)  
     is_featured = models.BooleanField(default=False)  
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
--- main/views.py
+++ main/views.py
@@ -4,26 +4,73 @@ from .forms import ProductForm
 from django.shortcuts import render, redirect ,get_object_or_404
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+@login_required(login_url="/login")
 def home(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(user=request.user)
     context = {
-        'app_name': 'Football Shop',
-        'student_name': 'Muhammad Rifqi Al Ghani',
-        'student_class': 'KKI',
-        'products': products,
-        'student_npm': '2406365396'
+        "app_name": "Football Shop",
+        "student_name": "Muhammad Rifqi Al Ghani",
+        "student_class": "KKI",
+        "student_npm": "2406365396",
+        "products": products,
+        "username": request.user.username,
+        "last_login": request.COOKIES.get("last_login"),
     }
     return render(request, "main.html", context)
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created successfully! Please log in.")
+            return redirect('main:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+
+            # set cookie last login
+            response = redirect('main:home')
+            response.set_cookie('last_login', str(timezone.now()))
+            return response
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_user(request):
+    logout(request)
+    response = redirect('main:login')
+    response.delete_cookie('last_login')
+    return response
+
+@login_required(login_url='/login/')
 def add_product(request):
     if request.method == "POST":
         form = ProductForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("main:show_main")
+            product = form.save(commit=False)
+            product.user = request.user   # assign ke user yang login
+            product.save()
+            return redirect("main:home")
     else:
-        form= ProductForm()
+        form = ProductForm()
     return render(request, "add_product.html", {"form": form})
+
 
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
